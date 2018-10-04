1d1c0fa26ba0946d1c48650355a22f5ab62d26d5
xruanjian <xruanjian@users.noreply.github.com>
2018-10-04 16:55:11 +0800

    Update swap.c

--- swap.c
+++ swap.c
@@ -1,5 +1,21 @@
 #include <stdio.h>
 
+//两个数因为是int基础类型，所以形参是传值，然后实例化的是一个备份并不会改变原来的值
+
+void swap(int a,int b){   
+    int m;    
+    m=a;  
+    *a=*b;     
+    *b=m;     
+}
+
+
+
+
+
+
+
+
 void swap(int *a,int *b){
     int m;
     m=*a;
