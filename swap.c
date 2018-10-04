4f43173f6a2c126be250272a995c6fa5b47d39bf
YiChen <xruanjian@gmail.com>
2018-10-04 17:02:36 +0800

    通过c语言的两数交换，理解传值和传址的区别，传值传的是备份，传址才会真正改变原参数值

--- swap.c
+++ swap.c
@@ -1,21 +1,25 @@
 #include <stdio.h>
 
-//两个数因为是int基础类型，所以形参是传值，然后实例化的是一个备份并不会改变原来的值
+/*
+两个错误示例
+swap1最容易犯错，因为int基础数据类型，作为形参是传值操作，相当于对a,b进行的备份进行操作，而不是传值操作,所以交换后，并不会改变原来的两个数
+void swap1(int a,int b){
+    int m;
+    m=a;
+    a=b;
+    b=m;
+    }
+    
+swap2交换的是指针的备份，也并不会改变两个数
+void swap2(int *a,int *b){
+    int *m;
+    m=*a;
+    *a=*b;
+    *b=m;
+    }
+*/
 
-void swap(int a,int b){   
-    int m;    
-    m=a;  
-    *a=*b;     
-    *b=m;     
-}
-
-
-
-
-
-
-
-
+//唯一正确的交换方法，采用指针作为形参进行传址操作，然后用一个实参值进行交换，最终真正改变两个值
 void swap(int *a,int *b){
     int m;
     m=*a;
