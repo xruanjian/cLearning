3c1e940facc5d4bea021459022c3fe6e8cf7caba
YiChen <xruanjian@gmail.com>
2018-10-28 01:06:48 +0800

    基础实例01这个是客户端socket实现的代码

--- socket编程/clientSocket.c
+++ socket编程/clientSocket.c
@@ -65,7 +65,7 @@ int main(int argc,char* argv[]){
 	}
 
 	sock=socket(PF_INET,SOCK_STREAM,0);
-	if(sock == -1){
+	if(sock == -1)
 		error_handling("socket()  error");
 
 	//memset：作用是在一段内存块中填充某个给定的值，
